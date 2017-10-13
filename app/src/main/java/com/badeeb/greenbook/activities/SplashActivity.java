11dbcb14b7a51f2ff36a12fb41e55571f4afc049
alghawy2010 <alghawy2010@gmail.com>
2017-10-13 16:29:39 +0300

    Splash Screen Creation

--- app/src/main/java/com/badeeb/greenbook/activities/SplashActivity.java
+++ app/src/main/java/com/badeeb/greenbook/activities/SplashActivity.java
@@ -1,15 +1,51 @@
 package com.badeeb.greenbook.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badeeb.greenbook.R;
+import com.badeeb.greenbook.shared.Constants;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final String TAG = SplashActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate - Start");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        int splashTimeOut = Constants.SPLASH_TIME_OUT;
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+
+                Log.d(TAG, "run - Start");
+
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+
+                // close this activity
+                finish();
+
+                Log.d(TAG, "run - End");
+            }
+        }, splashTimeOut);
+
+        Log.d(TAG, "onCreate - End");
     }
 }
---
+++ app/src/main/java/com/badeeb/greenbook/shared/Constants.java
@@ -0,0 +1,30 @@
+package com.badeeb.greenbook.shared;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Amr Alghawy on 6/12/2017.
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "https://drive-it-badeeb.herokuapp.com/api/v1";
+
+    // Volley constants
+    public static final int VOLLEY_TIME_OUT = 8000; // Milliseconds
+    public static final int VOLLEY_RETRY_COUNTER = 0;
+
+    // Location updates constants
+    public static final int UPDATE_TIME = 10 * 1000;    // Millisecoonds
+    public static final int UPDATE_DISTANCE = 10;        // meters
+
+    // Splash Screen timeout
+    public static final int SPLASH_TIME_OUT = 3000;
+}
