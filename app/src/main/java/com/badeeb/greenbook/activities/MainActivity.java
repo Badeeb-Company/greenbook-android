efa5bd9cf3f25d22e9ec091abdfa20cb48bbf642
alghawy2010 <alghawy2010@gmail.com>
2017-10-14 02:43:02 +0300

    login

--- app/src/main/java/com/badeeb/greenbook/activities/MainActivity.java
+++ app/src/main/java/com/badeeb/greenbook/activities/MainActivity.java
@@ -1,14 +1,21 @@
 package com.badeeb.greenbook.activities;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.badeeb.greenbook.R;
+import com.badeeb.greenbook.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = SplashActivity.class.getSimpleName();
+
+    private Toolbar mtoolbar;
+    private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
         Log.d(TAG, "onCreate - End");
     }
+
+    private void init() {
+        Log.d(TAG, "init - Start");
+
+        mFragmentManager = getSupportFragmentManager();
+
+        // Toolbar
+        mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+        goToLogin();
+
+        Log.d(TAG, "init - End");
+    }
+
+    private void goToLogin() {
+        LoginFragment loginFragment = new LoginFragment();
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_frame, loginFragment, loginFragment.TAG);
+        fragmentTransaction.commit();
+    }
+
 }
