0b889313ac15f60379bf6c10b72286accc00b37e
alghawy2010 <alghawy2010@gmail.com>
2017-10-13 16:37:12 +0300

    login fragment

---
+++ app/src/main/java/com/badeeb/greenbook/fragments/LoginFragment.java
@@ -0,0 +1,33 @@
+package com.badeeb.greenbook.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.badeeb.greenbook.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+    private final String TAG = LoginFragment.class.getSimpleName();
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return view;
+    }
+
+}
