7bdd61a880a4eedbbffcf83240c770865636acf8
alghawy2010 <alghawy2010@gmail.com>
2017-10-13 16:30:42 +0300

    logging

--- app/src/main/java/com/badeeb/greenbook/activities/MainActivity.java
+++ app/src/main/java/com/badeeb/greenbook/activities/MainActivity.java
@@ -2,14 +2,21 @@ package com.badeeb.greenbook.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badeeb.greenbook.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = SplashActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate - Start");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate - End");
     }
 }
