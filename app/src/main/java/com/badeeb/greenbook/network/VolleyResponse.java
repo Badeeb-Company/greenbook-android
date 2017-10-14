6deb7f57b537a00a6f1a1a05d4d369ffc47328f8
alghawy2010 <alghawy2010@gmail.com>
2017-10-15 01:17:45 +0300

    Volley Wrapper Implementation

---
+++ app/src/main/java/com/badeeb/greenbook/network/VolleyResponse.java
@@ -0,0 +1,12 @@
+package com.badeeb.greenbook.network;
+
+import com.badeeb.greenbook.models.JsonMeta;
+
+/**
+ * Created by Amr Alghawy on 10/15/2017.
+ */
+
+public interface VolleyResponse<S> {
+
+    public JsonMeta getJsonMeta();
+}
